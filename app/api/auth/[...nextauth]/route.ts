ee08437a50709503a86e823ad934243c82292c5b
Your Name <you@example.com>
2026-03-20 00:08:57 +0200

    fix: next-auth app router handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth/next"
 import { authOptions } from "@/lib/auth"
 
 // تمرير خيارات إضافية لتخطي مشكلة (Host Trust Validation) و CSRF
